36179b70ed012a44cfeb76127a54bb75f0fab22e
lfhernandez03 <Luishsg1@gmail.com>
2024-08-24 12:59:07 -0500

    Login

---
+++ src/views/components/login.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button } from './button';
+const Login = () => {
+    return (
+        <div className='grid justify-center gap-2 border p-4 m-4 rounded' >
+            <div className='pb-4'>
+                <h1 className='underline flex justify-center'>Login</h1>
+            </div>
+            <div className='flex justify-center'>
+                <Button text='Ingresar con gugulu' className='  bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full' />
+            </div>
+            <div className='flex items-center'>
+                <div className='flex-grow border-t border-gray-300'/>
+                <span className='text-xs flex justify-center p-2'>O ingresar con Email</span>
+                <div className='flex-grow border-t border-gray-300'/>
+            </div>
+            <div className=''>
+                <form>
+                     <div className=''>
+                        <div className='py-2'>
+                            <label htmlFor='email' className=''>Email</label>
+                        </div>
+                        <div>  
+                            <input type='email' id='email' name='email' placeholder='  golis' className='border rounded  ' /> 
+                        </div>
+                     </div>
+                     <div className=' py-2'>
+                        <div className=''>
+                            <label htmlFor='password' className=''>Contraseña</label>
+                        </div>
+                        <div>  
+                            <input type='password' id='password' name='password' placeholder='  golis' className='border rounded ' /> 
+                        </div>
+                     </div>
+
+                </form>
+            </div> 
+        </div>
+    );
+}
+
+export { Login };
--- src/views/layouts/mainLayout.jsx
+++ src/views/layouts/mainLayout.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 import { Button } from '../components/button';
+import { Login } from '../components/login';
 import '/src/index.css';
 
 const MainLayout = ({ children }) => {
     return (
         <div className='flex justify-center'>
-            <Button text='Click me' className='bg-red-500 border-2 border-red-600 rounded-full px-4 py-1 hover:bg-red-600 '/>
+            <Login></Login>
         </div>
     );
 };
