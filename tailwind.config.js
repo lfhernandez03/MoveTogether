3f6988bab21f2413b509d2a9eb63c4e0a661fc4f
lfhernandez03 <Luishsg1@gmail.com>
2024-09-04 10:11:41 -0500

    solucion de errores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /* @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/**/*.{js,jsx,ts,tsx}",
-];
-export const theme = {
-  extend: {
-    boxShadow: {
-      'custom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06), 0 10px 20px rgba(0, 128, 0, 0.5)', // Ejemplo de sombra con color verde
+
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06), 0 10px 20px rgba(0, 128, 0, 0.5)', // Ejemplo de sombra con color verde
+      },
     },
   },
+  plugins: [],
 };
-export const variants = {};
-export const plugins = [];
