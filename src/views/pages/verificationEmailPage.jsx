72537c0ae36f35670b28a64449d45759589e11bf
Dieguit0000 <diegofernandomorenomartinez@gmail.com>
2024-09-28 00:36:57 -0500

    Navbar HomePage
    Se creo la Page de Home (para que no la vuelvan a crear)

---
+++ src/views/pages/verificationEmailPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import VerificationEmail from '../auth/components/verificationEmail';
+
+const Si = () => {
+  return (
+    <VerificationEmail />
+  )
+}
+
+export default Si;
