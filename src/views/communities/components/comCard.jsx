5c51856138803a1744ebad11818a95644d5c4f43
lfhernandez03 <Luishsg1@gmail.com>
2024-12-17 22:54:27 -0500

    ComCard

--- src/views/communities/components/comCard.jsx
+++ src/views/communities/components/comCard.jsx
@@ -1,7 +1,33 @@
 import React, { memo } from "react";
 import Button from "../../global/elements/button";
+import axios from "axios";
 
-const ComCard = ({ imagenPerfil, nombre, administradorId, categorias, miembros, descripcion}) => {
+const token = localStorage.getItem("authToken");
+
+const handleJoin = async (event) => {
+  event.prevent.default;
+  try {
+    const response = await axios.post("http://localhost:5000/api/unirse/comunidad/", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.status === 200) {
+      console.log("Se ha unido a la comunidad");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const ComCard = ({
+  imagenPerfil,
+  nombre,
+  administradorId,
+  categorias,
+  miembros,
+  descripcion,
+}) => {
   return (
     <div className="flex w-128 bg-slate-100 justify-center p-4">
       <div className="flex flex-col p-4 rounded-xl bg-white w-96 justify-center gap-4">
@@ -18,24 +44,26 @@ const ComCard = ({ imagenPerfil, nombre, administradorId, categorias, miembros, 
         </div>
         <div className="flex py-4">
           {categorias.map((categoria) => (
-            <div key={categoria} className="rounded-full bg-green-200 px-4 text-sm font-semibold">
+            <div
+              key={categoria}
+              className="rounded-full bg-green-200 px-4 text-sm font-semibold"
+            >
               {categoria}
             </div>
           ))}
         </div>
         <div>
-        <p className="text-sm">{descripcion}</p>
+          <p className="text-sm">{descripcion}</p>
         </div>
         <div className="flex justify-between items-center">
-        <div>
-          <p>
-            Miembros: {miembros.length}
-          </p>
-        </div>
+          <div>
+            <p>Miembros: {miembros.length}</p>
+          </div>
           <div>
             <Button
               text="Unirse"
               className="bg-blue-500 text-white rounded-full"
+              onClick={handleJoin}
             ></Button>
           </div>
         </div>
